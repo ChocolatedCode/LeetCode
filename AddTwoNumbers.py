a09b9cf6cd54d3617fec923cb40d519c4c27c481
ChocolatedCode <71442984+ChocolatedCode@users.noreply.github.com>
2021-08-15 17:29:43 +0200

    Update AddTwoNumbers.py

--- AddTwoNumbers.py
+++ AddTwoNumbers.py
@@ -14,25 +14,29 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
+        #base cases to check if one given list is empty
         if l1 is None:
             return l2
         elif l2 is None:
             return l1
         
-        res = l1
+        res = l1 #reference to head of listNode l1
         rest = 0
         previous = None
-        while l1 and l2:
-            l1.val += l2.val + rest
+        
+        while l1 and l2: # O(n) time 
+            l1.val += l2.val + rest # modification of l1 list values directly
             #print(l1.val)
-            rest = int(l1.val/10)
+            rest = int(l1.val/10) 
             l1.val = l1.val%10
             previous = l1
-            print(previous)
-            print(res)
+            # print(previous)
+            # print(res)
+            #go to next nodes
             l1 = l1.next
             l2 = l2.next
         
+        #is there remaining numbers
         if l1:
             while l1:
                 l1.val += rest
@@ -50,10 +54,11 @@ class Solution(object):
                 previous = l2
                 l2 = l2.next
                 
+        #last remaining rest      
         if rest != 0:
             previous.next = ListNode(rest)
         
-        return res
+        return res # O(n) space 
            
         
         
